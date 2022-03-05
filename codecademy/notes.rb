a48d9eef6084e2606696bf61f19b51c36cb40d44
DaCuteRaccoon <72467156+DaCuteRaccoon@users.noreply.github.com>
2022-03-05 15:19:21 -0800

    Create Codecademy notes

---
+++ codecademy/notes.rb
@@ -0,0 +1,46 @@
+# Important notes from codecademy
+puts 'This file is currently commented out to prevent CPU straining.'
+=begin
+
+
+
+my_num = 25
+my_boolean = true
+my_string = "Ruby"
+
+puts my_num
+puts my_boolean
+puts my_string
+
+5 + 3
+17 - 9
+2 * 4
+32 / 4
+2 ** 3
+8 % 17
+
+puts "What's up?"
+print "Oxnard Montalvo"
+
+"DaCuteRaccoon".length
+
+"DaCuteRaccoon".reverse
+
+puts "DaCuteRaccoon".upcase
+puts "DaCuteRaccoon".downcase
+
+name = "DaCuteRaccoon"
+
+my_name = "DaCuteRaccoon"
+my_age = 100
+
+sum = 13 + 379
+product = 923 * 15
+quotient = 13209 / 17
+
+name = "DaCuteRaccoon"
+puts name.downcase.reverse.upcase
+
+
+
+=end
