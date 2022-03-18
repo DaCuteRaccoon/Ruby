97b43ca0b92131787f3c68545aeb397f5819661a
DaCuteRaccoon stands with Ukraine <72467156+DaCuteRaccoon@users.noreply.github.com>
2022-03-17 18:34:31 -0700

    Add `nodejs`, `yarn`, and `xdg-utils` to replit.nix

--- replit.nix
+++ replit.nix
@@ -1,7 +1,10 @@
 { pkgs }: {
 	deps = [
-        pkgs.ruby
-        pkgs.solargraph
-        pkgs.rufo
+		pkgs.ruby
+		pkgs.solargraph
+		pkgs.rufo
+		pkgs.nodejs
+		pkgs.yarn
+		pkgs.xdg-utils
 	];
 }
