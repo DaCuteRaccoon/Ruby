fdbebfce49ab81739e95214674ae6180beef8148
DaCuteRaccoon stands with Ukraine <72467156+DaCuteRaccoon@users.noreply.github.com>
2022-03-17 18:44:10 -0700

    Create main.sh for an alternate way of executing main.rb

---
+++ main.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+bundle exec ruby main.rb
+
+
+# https://stackoverflow.com/questions/71389408/why-is-a-comment-in-bash/
+
+comment () {}
+comment echo 'You will never see this message'
